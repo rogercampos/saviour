d56127dfdec631b1d712898ea115db6af447d00e
Roger Campos <roger@itnig.net>
2015-05-13 00:17:57 +0200

    Improve UrlSource and add specs

--- lib/saviour/url_source.rb
+++ lib/saviour/url_source.rb
@@ -1,14 +1,20 @@
-require 'open-uri'
 require 'uri'
 
 module Saviour
   class UrlSource
     def initialize(url)
-      @uri = URI(url)
+      begin
+        @uri = URI(url)
+      rescue URI::InvalidURIError
+        raise ArgumentError, "'#{url}' is not a valid URI"
+      end
     end
 
     def read
-      open(@uri.to_s).read
+      response = with_retry(3) { Net::HTTP.get_response(@uri) }
+
+      raise("Request to #{@uri} failed after 3 attempts.") unless response.is_a?(Net::HTTPSuccess)
+      response.body
     end
 
     def original_filename
@@ -17,6 +23,19 @@ module Saviour
 
     def path
       @uri.path
+    end
+
+
+    private
+
+    def with_retry(n = 3, &block)
+      res = block.call
+
+      if res.is_a?(Net::HTTPSuccess) || n == 0
+        res
+      else
+        with_retry(n - 1, &block)
+      end
     end
   end
 end
