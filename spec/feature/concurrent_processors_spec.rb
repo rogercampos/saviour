d3b2ed3fd7e6117b2c2b133360d0767181478bff
Roger Campos <roger@rogercampos.com>
2018-03-07 19:39:13 -0500

    Adjust tests to get consistent builds in travis

--- spec/feature/concurrent_processors_spec.rb
+++ spec/feature/concurrent_processors_spec.rb
@@ -3,23 +3,36 @@ require 'spec_helper'
 describe "concurrent processors" do
   before { allow(Saviour::Config).to receive(:storage).and_return(Saviour::LocalStorage.new(local_prefix: @tmpdir, public_url_prefix: "http://domain.com")) }
 
-  WAIT_TIME = 1
-  WITHIN_MARGIN = WAIT_TIME * 0.2 # 20% margin
+  WAIT_TIME = 0.5
 
   let(:uploader) {
     Class.new(Saviour::BaseUploader) {
       store_dir { "/store/dir" }
 
       process_with_file do |file, filename|
+        stash(attached_as => Time.now.to_f)
         sleep WAIT_TIME
 
         [file, filename]
+      end
+
+      after_upload do |stash|
+        model.times = model.times.merge(stash)
       end
     }
   }
 
   let(:klass) {
-    klass = Class.new(Test) { include Saviour::Model }
+    klass = Class.new(Test) {
+      include Saviour::Model
+
+      attr_accessor :times
+
+      def initialize(*)
+        super
+        @times = {}
+      end
+    }
     klass.attach_file :file, uploader
     klass.attach_file :file_thumb, uploader
     klass.attach_file :file_thumb_2, uploader
@@ -33,15 +46,13 @@ describe "concurrent processors" do
 
       Saviour::Config.concurrent_workers = 4
 
-      t0 = Time.now
       a.update_attributes! file: Saviour::StringSource.new("contents", "file.txt"),
                            file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
                            file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
                            file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      diff = Time.now - t0
-      expect(diff).to be_within(WITHIN_MARGIN).of(WAIT_TIME)
-      expect(diff).to be < WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[-1]).abs).to be_within(0.1).of(0)
     end
 
     it 'works in serial with 1 worker' do
@@ -49,13 +60,13 @@ describe "concurrent processors" do
 
       Saviour::Config.concurrent_workers = 1
 
-      t0 = Time.now
       a.update_attributes! file: Saviour::StringSource.new("contents", "file.txt"),
                            file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
                            file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
                            file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      expect(Time.now - t0).to be >= WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[-1]).abs).to be > WAIT_TIME * 3
     end
 
     it 'concurrency can be adjusted' do
@@ -63,15 +74,15 @@ describe "concurrent processors" do
 
       Saviour::Config.concurrent_workers = 2
 
-      t0 = Time.now
       a.update_attributes! file: Saviour::StringSource.new("contents", "file.txt"),
                            file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
                            file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
                            file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      diff = Time.now - t0
-      expect(diff).to be_within(WITHIN_MARGIN).of(WAIT_TIME * 2)
-      expect(diff).to be < WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[1]).abs).to be_within(0.1).of(0)
+      expect((start_times[2] - start_times[3]).abs).to be_within(0.1).of(0)
+      expect((start_times[0] - start_times[-1]).abs).to be > WAIT_TIME
     end
   end
 
@@ -79,41 +90,39 @@ describe "concurrent processors" do
     it 'works concurrently with 4 workers' do
       Saviour::Config.concurrent_workers = 4
 
-      t0 = Time.now
-      klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
-                    file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
-                    file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
-                    file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
+      a = klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
+                        file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
+                        file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
+                        file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      diff = Time.now - t0
-      expect(diff).to be_within(WITHIN_MARGIN).of(WAIT_TIME)
-      expect(diff).to be < WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[-1]).abs).to be_within(0.1).of(0)
     end
 
     it 'works in serial with 1 worker' do
       Saviour::Config.concurrent_workers = 1
 
-      t0 = Time.now
-      klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
-                    file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
-                    file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
-                    file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
+      a = klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
+                        file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
+                        file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
+                        file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      expect(Time.now - t0).to be >= WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[-1]).abs).to be > WAIT_TIME * 3
     end
 
     it 'concurrency can be adjusted' do
       Saviour::Config.concurrent_workers = 2
 
-      t0 = Time.now
-      klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
-                    file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
-                    file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
-                    file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
+      a = klass.create! file: Saviour::StringSource.new("contents", "file.txt"),
+                        file_thumb: Saviour::StringSource.new("contents", "file_2.txt"),
+                        file_thumb_2: Saviour::StringSource.new("contents", "file_3.txt"),
+                        file_thumb_3: Saviour::StringSource.new("contents", "file_4.txt")
 
-      diff = Time.now - t0
-      expect(diff).to be_within(WITHIN_MARGIN).of(WAIT_TIME * 2)
-      expect(diff).to be < WAIT_TIME * 4
+      start_times = a.times.values.sort
+      expect((start_times[0] - start_times[1]).abs).to be_within(0.1).of(0)
+      expect((start_times[2] - start_times[3]).abs).to be_within(0.1).of(0)
+      expect((start_times[0] - start_times[-1]).abs).to be > WAIT_TIME
     end
   end
 end
